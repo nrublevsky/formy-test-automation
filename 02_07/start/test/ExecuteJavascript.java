aed2a18a908e7dd0ff9b13e947f2aab468135797
nrublevsky <nickrublevsky@gmail.com>
2023-08-06 00:05:16 +0200

    wefwef

--- 02_07/start/test/ExecuteJavascript.java
+++ 02_07/start/test/ExecuteJavascript.java
@@ -7,11 +7,20 @@ import org.openqa.selenium.By;
 public class ExecuteJavascript {
     public static void main(String[] args) {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:/Nick Stuff/Work/Selenium stuff/chromedriver/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/modal");
+
+        WebElement modalButton = driver.findElement(By.id("modal-button"));
+        modalButton.click();
+
+        WebElement closeButton = driver.findElement(By.id("close-button"));
+
+        JavascriptExecutor js = (JavascriptExecutor)driver;
+
+        js.executeScript("arguments[0].click();",closeButton);
 
         driver.quit();
     }
--- 02_08/start/test/DragAndDrop.java
+++ 02_08/start/test/DragAndDrop.java
@@ -5,13 +5,22 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.interactions.Actions;
 
 public class DragAndDrop {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
 
-        System.setProperty("webdriver.chrome.driver", "/Users/meaghanlewis/Downloads/chromedriver");
+        System.setProperty("webdriver.chrome.driver", "C:/Nick Stuff/Work/Selenium stuff/chromedriver/chromedriver.exe");
 
         WebDriver driver = new ChromeDriver();
 
         driver.get("https://formy-project.herokuapp.com/dragdrop");
+
+        WebElement image = driver.findElement(By.id("image"));
+
+        WebElement box = driver.findElement(By.id("box"));
+
+        Actions actions = new Actions(driver);
+        actions.dragAndDrop(image,box).build().perform();
+
+        Thread.sleep(5000);
 
         driver.quit();
     }
